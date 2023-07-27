bd3abcc84dbb9656b7ed394a4432bed8e7002753
awaykim <121528605+awaykim@users.noreply.github.com>
2023-07-27 18:59:22 +0900

    [Silver I] Title: 카드 구매하기, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,17 @@
+N = int(input())
+P = list(map(int, input().split()))
+P.insert(0,0)
+def solution(arr):
+    n = len(arr)
+    dp = [0] * (n)
+    dp[1] = arr[1]
+    for i in range(2, n):
+        r = i // 2
+        tmp = []
+        tmp.append(arr[i])
+        for j in range(1, r+1):
+            tmp.append(dp[j]+dp[i-j])
+        dp[i] = max(tmp)
+    return(dp[n-1])
+
+print(solution(P))
