a67a61804b142568ed8fb56de39755ec0e3161d1
awaykim <121528605+awaykim@users.noreply.github.com>
2023-08-10 13:29:40 +0900

    [Gold V] Title: 신기한 소수, Time: 48 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.py
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -1,21 +1,20 @@
 import sys, math
 n = int(sys.stdin.readline())
-
-def solution(n):
+def solution_2(n):
     def is_prime(n):
         for i in range(2, int(math.sqrt(n))+1):
             if n % i == 0: return False
         return True
-    primes = (2, 3, 5, 7)
-    for _ in range(n-1):
-        tmp = []
-        for p in primes:
-            for i in range(1, 10, 2):
-                w = p * 10 + i
-                if is_prime(w): tmp.append(w)
-        primes = tuple(tmp)
-    for i in primes:
-        print(i)
+    def dfs(num):
+        if len(str(num)) == n: print(num)
+        for i in range(1, 10, 2):
+            candidate = num * 10 + i
+            if is_prime(candidate): dfs(candidate)
+        return
+    dfs(2)
+    dfs(3)
+    dfs(5)
+    dfs(7)
     return
 
-solution(n)
+solution_2(n)
