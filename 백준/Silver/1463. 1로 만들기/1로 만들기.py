bfec0e7bd92cb5cfe7665b07fa5da39c47d651eb
awaykim <121528605+awaykim@users.noreply.github.com>
2023-08-31 19:37:07 +0900

    [Silver III] Title: 1로 만들기, Time: 816 ms, Memory: 79064 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,17 +1,10 @@
 import sys
-N = int(input())
-
-def solution(n):
-    table = [0] * (n + 1)
-    for i in range(1, len(table)):
-        table[i] = i - 1
-    for i in range(2, len(table)):
-        sub_res = [sys.maxsize] * 3
-        if i % 3 == 0: sub_res[0] = table[i//3] + 1
-        if i % 2 == 0 : sub_res[1] = table[i//2] + 1
-        sub_res[2] = table[i-1] + 1
-        for j in range(3):
-            table[i] = min(table[i], sub_res[j])
-    return table[n]
-
-print(solution(N))
+n = int(sys.stdin.readline())
+dp = [0] + [(i-1) for i in range(1, n+1)]
+for i in range(2, len(dp)):
+    sub_res = [sys.maxsize] * 3
+    if i % 3 == 0: sub_res[0] = dp[i//3] + 1
+    if i % 2 == 0 : sub_res[1] = dp[i//2] + 1
+    sub_res[2] = dp[i-1] + 1
+    dp[i] = min(sub_res)
+print(dp[n])
