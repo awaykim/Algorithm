559d5abae01a127d108ba0aa3051fa7003eedba5
awaykim <121528605+awaykim@users.noreply.github.com>
2023-10-18 20:30:50 +0900

    [Silver IV] Title: 보물, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,7 @@
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+s = 0
+for i in range(n):
+    s += a.pop(a.index(min(a))) * b.pop(b.index(max(b)))
+print(s)
