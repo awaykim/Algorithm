48e6016b6ac220e5fcdfc8e9d04dc0b97ddc3c2a
awaykim <121528605+awaykim@users.noreply.github.com>
2024-03-11 16:02:18 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -4,16 +4,11 @@ for i in range(26): vector[i] = name.count(chr(ord('A')+i))
 string = ''
 mid = ''
 for i in range(26):
-    if vector[i] == 0: continue
-    if vector[i] % 2 != 0:
-        if len(name) % 2 == 0: 
+    if vector[i] % 2 == 1:
+        if mid != '':
             print('I\'m Sorry Hansoo')
             exit()
-        else:
-            if mid =='': mid = chr(ord('A')+i)
-            else:
-                print('I\'m Sorry Hansoo')
-                exit()
-    string = string + chr(ord('A')+i) * (vector[i] // 2)
+        mid = chr(ord('A')+i)
+    string += chr(ord('A')+i) * (vector[i] // 2)
 ans = string + mid + string[::-1]
 print(ans)
