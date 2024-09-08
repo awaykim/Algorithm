0a7a5bba8672022301e78ee8f4d1308341880baf
awaykim <121528605+awaykim@users.noreply.github.com>
2024-09-08 17:42:31 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,6 +1,16 @@
-import itertools
+ans = []
 n, m = map(int, input().split())
-arr = [i for i in range(1, n+1)]
-nCm = list(itertools.combinations(arr, m))
-for i in range(len(nCm)):
-    print(*nCm[i])
+# dfs_1()
+
+# 1~N 자연수 중 중복없이 M개를 고른 오름차순 수열
+def dfs_2(start):
+    if len(ans) == m:
+        print(*ans)
+        return 
+    for i in range(start, n+1):
+        if i in ans: continue
+        ans.append(i)
+        dfs_2(i)
+        ans.pop()
+
+dfs_2(1)
