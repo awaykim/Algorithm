ac27fde59daff9a604de66ed28e4e952704f45ae
awaykim <121528605+awaykim@users.noreply.github.com>
2024-09-08 17:38:13 +0900

    [Silver III] Title: N과 M (1), Time: 172 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,7 +1,13 @@
-import itertools
+def dfs_1():
+    if len(ans) == m:
+        print(*ans)
+        return 
+    for i in range(1, n+1):
+        if i in ans: continue
+        ans.append(i)
+        dfs_1()
+        ans.pop()
+
+ans = []
 n, m = map(int, input().split())
-arr = [i for i in range(1, n+1)]
-nPm = list(itertools.permutations(arr, m))
-for i in range(len(nPm)):
-    print(*nPm[i], end="")
-    print()
+dfs_1()
