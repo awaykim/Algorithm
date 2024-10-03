2da03ab74e1efc6376c1f9068c8a72f71f2dec74
awaykim <121528605+awaykim@users.noreply.github.com>
2024-10-03 17:07:06 +0900

    [level 3] Title: 베스트앨범, Time: 0.09 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -0,0 +1,25 @@
+def solution(genres, plays):
+    answer = []
+    # dictionary 선언 
+    songs = {}
+    gen_counts = {}
+    # 장르 별로 모으기
+    for i in range(len(genres)):
+        genre = genres[i]
+        play = plays[i]
+        if genre not in gen_counts:
+            gen_counts[genre] = 0
+            songs[genre] = []
+        gen_counts[genre] += play
+        songs[genre].append((play, i))
+    # 장르 정렬 
+    sorted_gen = sorted(gen_counts.keys(), key=lambda x: gen_counts[x], reverse=True)
+    # 장르 내에서 정렬
+    for genre in songs:
+        songs[genre].sort(key=lambda x: (-x[0], x[1]))
+    # 최상위 2개까지만 수록
+    for genre in sorted_gen:
+        answer.append(songs[genre][0][1])
+        if len(songs[genre]) > 1:
+            answer.append(songs[genre][1][1])
+    return answer
