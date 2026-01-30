7274a96f4af3dc8f97bde925f4f42919e1d760ce
Wonjeong Kim <121528605+awaykim@users.noreply.github.com>
2026-01-30 10:48:09 +0900

    [Silver I] Title: IOIOI, Time: 116 ms, Memory: 112428 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,9 +1,32 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 M = int(input())
 S = input()
-target = "" 
-answer = 0
-length = 2 * N + 1
-for i in range(length): target += "O" if i % 2 else "I"
-for i in range(len(S)): answer += 1 if S[i:length+i] == target else 0
-print(answer)
+P = "IOI"
+i = 0
+j = 3
+ans = 0
+Pcontinue = 0
+
+while True:
+  if j >= len(S): break
+
+  if S[i:j] == P: 
+    Pcontinue += 1
+    if Pcontinue < N: 
+      i += 2
+      j += 2
+      continue
+    else:
+      ans += 1
+      Pcontinue -= 1
+      i += 2
+      j += 2
+  else:
+    i += 1
+    j += 1
+    Pcontinue = 0
+
+print(ans)
