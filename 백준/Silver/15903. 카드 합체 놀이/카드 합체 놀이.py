a30bd74a5c87819f705622910a5de428de2f1678
Wonjeong Kim <121528605+awaykim@users.noreply.github.com>
2026-02-25 17:11:15 +0900

    [Silver II] Title: 카드 합체 놀이, Time: 136 ms, Memory: 111948 KB -BaekjoonHub

---
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
@@ -0,0 +1,12 @@
+import heapq
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+heapq.heapify(arr)
+for _ in range(m):
+  card1 = heapq.heappop(arr)
+  card2 = heapq.heappop(arr)
+  heapq.heappush(arr, card1+card2)
+  heapq.heappush(arr, card1+card2)
+
+print(sum(arr))
